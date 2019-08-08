43c769c088f29bde6ee49111f2fef9babeb04388
unknown <Calin Cornigeanu>
2019-08-08 19:33:01 +0300

    debugging

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -8,7 +8,7 @@ export class IndexController {
       jobsIndex: '.arcanna-jobs',
       jobsIndexType: '_doc'
     };
-    console.log(JSON.stringify(server.config().get("hosts")));
+    console.log(JSON.stringify(server.config().get(), null, 2));
     this.esClient = new Client({
   
       // host: server.config().get('elasticsearch.hosts')[0]
