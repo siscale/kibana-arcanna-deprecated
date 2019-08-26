2656be2bc7a80d5bf082ed6ea24de15bc2efae3a
unknown <Calin Cornigeanu>
2019-08-26 19:57:56 +0300

    debugging

--- public/app.js
+++ public/app.js
@@ -40,7 +40,7 @@ import { HomepageController } from './controllers/homepage'
 function RootController($scope, $element, $http) {
   const domNode = $element[0];
   render(
-    <H1>Hello?</H1>
+    // <H1>Hello?</H1>
     <Il8nProvider>
       <H2>Hello2?</H2>
       <BrowserRouter>
