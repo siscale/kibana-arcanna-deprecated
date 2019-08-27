79fe789896936d5c07c2b038ffd41194800813ce
unknown <Calin Cornigeanu>
2019-08-27 14:09:46 +0300

    debugging

--- public/app.js
+++ public/app.js
@@ -24,9 +24,8 @@ import './services/factories';
 import './controllers/index';
 import './services/routes';
 
-await sleep(2000);
+setTimeout(() => { window.location.href = '#/'; }, 2000)
 
-window.location.href = '#/';
 
 import { HomepageController } from './controllers/homepage'
 import homepageTemplate from './templates/homepage.html';
