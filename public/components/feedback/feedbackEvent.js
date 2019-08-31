1831aa208848f8315ff2896c40e2a92c7cc873cc
unknown <Calin Cornigeanu>
2019-08-31 15:59:47 +0300

    fixing feedback_event

--- public/components/feedback/feedbackEvent.js
+++ public/components/feedback/feedbackEvent.js
@@ -43,7 +43,27 @@ export class FeedbackEvent extends React.Component {
       }
     };
     this.genericRequest = new GenericRequest();
+    // this.feedbackStatusMapping = {
+    //   SYMPTOM: {
+    //     id: "SYMPTOM",
+    //     color: 'warning',
+    //     displayName: "Symptom",
+    //     checked: false
+    //   },
+    //   ROOT_CAUSE: {
+    //     id: "ROOT_CAUSE",
+    //     displayName: "Root cause",
+    //     color: 'danger',
+    //     checked: true
+    //   }
+    // }
     this.feedbackStatusMapping = {
+      IRRELEVANT: {
+        id: "IRRELEVANT",
+        color: 'default',
+        displayName: "Irrelevant",
+        checked: false
+      },
       SYMPTOM: {
         id: "SYMPTOM",
         color: 'warning',
@@ -56,6 +76,16 @@ export class FeedbackEvent extends React.Component {
         color: 'danger',
         checked: true
       }
+    }
+  }
+
+  classToNameMapping(cls) {
+    if(cls === -1) {
+      return "IRRELEVANT";
+    } else if(cls === 0) {
+      return "SYMPTOM";
+    } else if(cls === 1) {
+      return "ROOT_CAUSE";
     }
   }
 
@@ -72,19 +102,19 @@ export class FeedbackEvent extends React.Component {
       indexName: this.props.event.arcanna.source_index,
       documentContent: JSON.stringify(source, null, 2),
       arcanna: this.props.event.arcanna,
-      // status: this.feedbackStatusMapping[this.props.event.arcanna.arcanna_class]
-      status: this.feedbackStatusMapping["SYMPTOM"]
+      status: this.feedbackStatusMapping[this.classToNameMapping(this.props.event.arcanna.best_match)]
+      // status: this.feedbackStatusMapping["SYMPTOM"]
     });
   }
 
   onChangeSwitch = () => {
-    if(this.state.status.id === "SYMPTOM") {
+    if (this.state.status.id === "SYMPTOM") {
       const newStatus = this.feedbackStatusMapping.ROOT_CAUSE;
-      this.setState({status: newStatus})
+      this.setState({ status: newStatus })
       this.props.onSwitchChange(this.state.indexName, this.state.id, newStatus.id);
     } else {
       const newStatus = this.feedbackStatusMapping.SYMPTOM;
-      this.setState({status: newStatus});
+      this.setState({ status: newStatus });
       this.props.onSwitchChange(this.state.indexName, this.state.id, newStatus.id);
     }
   }
@@ -93,20 +123,20 @@ export class FeedbackEvent extends React.Component {
     const accordionContent = (
       <EuiFlexGroup gutterSize="s">
         <EuiFlexItem grow={false}>
-        <EuiBadge>{this.state.indexName}</EuiBadge>
+          <EuiBadge>{this.state.indexName}</EuiBadge>
         </EuiFlexItem>
         <EuiFlexItem>
           <EuiText>
             <h4>{this.state.id}</h4>
           </EuiText>
         </EuiFlexItem>
-        
+
       </EuiFlexGroup>
     );
     return (
-      
+
       <EuiFlexGroup>
-        <EuiFlexItem style={{minWidth:500}} grow={false}>
+        <EuiFlexItem style={{ minWidth: 500 }} grow={false}>
           <EuiAccordion buttonContent={accordionContent}>
             <EuiCodeBlock language="json">
               {this.state.documentContent}
@@ -117,10 +147,10 @@ export class FeedbackEvent extends React.Component {
           </EuiText> */}
         </EuiFlexItem>
         <EuiFlexItem grow={false}>
-        <EuiSwitch 
-          checked={this.state.status.checked}
-          onChange={this.onChangeSwitch}
-        />
+          <EuiSwitch
+            checked={this.state.status.checked}
+            onChange={this.onChangeSwitch}
+          />
         </EuiFlexItem>
         <EuiFlexItem grow={false}>
           <EuiBadge color={this.state.status.color}>
