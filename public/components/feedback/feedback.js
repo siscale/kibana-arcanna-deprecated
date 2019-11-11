fe09fe8db7c41eae87121b3d7d1f1833f427893d
Calin Cornigeanu <alex.cornigeanu@yahoo.com>
2019-11-11 16:12:08 +0200

    added title. debugging

--- public/components/feedback/feedback.js
+++ public/components/feedback/feedback.js
@@ -143,6 +143,16 @@ export class FeedbackComponent extends React.Component {
     }
   }
 
+  renderFeedbackBatchTitle() {
+    if(this.state.events.length > 0) {
+      return (
+        <EuiText>
+          <h3>Incident {this.state.events[0].arcanna.batch_id}</h3>
+        </EuiText>
+      );
+    }
+  }
+
   renderFeedbackElements() {
     var rows = [];
     this.state.events.forEach((event) => {
@@ -268,6 +278,9 @@ export class FeedbackComponent extends React.Component {
                 </EuiFlexGroup>
               </EuiFlexItem>
             </EuiFlexGroup>
+          </EuiFlexItem>
+          <EuiFlexItem>
+            {this.renderFeedbackBatchTitle()}
           </EuiFlexItem>
           <EuiFlexItem>
             <EuiFlexGrid>
