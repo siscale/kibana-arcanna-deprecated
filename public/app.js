50c1693be0118594ca6a3782a892d4a8bc7ad78d
Calin Cornigeanu <alex.cornigeanu@yahoo.com>
2020-03-09 19:23:33 +0200

    testing react router

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
 import { uiModules } from 'ui/modules';
 import chrome from 'ui/chrome';
 import 'ui/autoload/styles';
