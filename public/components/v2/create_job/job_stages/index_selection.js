80023b171cb227a7b7feda44a78955f1dd3b1d47
Calin Cornigeanu <alex.cornigeanu@yahoo.com>
2020-03-11 13:38:37 +0200

    adding index selection

--- public/components/v2/create_job/job_stages/index_selection.js
+++ public/components/v2/create_job/job_stages/index_selection.js
@@ -25,85 +25,85 @@ export class IndexSelection extends Component {
       stillLoading: true,
       selectedItems: []
     };
-    this.setColumnInfo();
+    // this.setColumnInfo();
   }
 
-  static propTypes = {
-    selectedIndexList: PropTypes.array
-  }
+  // static propTypes = {
+  //   selectedIndexList: PropTypes.array
+  // }
 
-  setColumnInfo() {
-    this.columns = [{
-      field: 'index_name',
-      name: 'Index Name',
-      sortable: true
-    }, {
-      field: 'state',
-      name: 'State',
-      sortable: true,
-      render: (state) => {
-        const color = state ? 'success' : 'danger';
-        const label = state ? 'Open' : 'Closed';
-        return <EuiHealth color={color}>{label}</EuiHealth>;
-      }
-    }, {
-      field: 'creation_date',
-      name: 'Creation Date',
-      dataType: 'date',
-      sortable: true,
-      render: (date) => moment(date).toISOString()
-    }, {
-      field: 'number_of_shards',
-      name: 'Number of Shards',
-      sortable: false
-    }, {
-      field: 'number_of_replicas',
-      name: 'Number of Replicas',
-      sortable: false
-    }];
-  }
+  // setColumnInfo() {
+  //   this.columns = [{
+  //     field: 'index_name',
+  //     name: 'Index Name',
+  //     sortable: true
+  //   }, {
+  //     field: 'state',
+  //     name: 'State',
+  //     sortable: true,
+  //     render: (state) => {
+  //       const color = state ? 'success' : 'danger';
+  //       const label = state ? 'Open' : 'Closed';
+  //       return <EuiHealth color={color}>{label}</EuiHealth>;
+  //     }
+  //   }, {
+  //     field: 'creation_date',
+  //     name: 'Creation Date',
+  //     dataType: 'date',
+  //     sortable: true,
+  //     render: (date) => moment(date).toISOString()
+  //   }, {
+  //     field: 'number_of_shards',
+  //     name: 'Number of Shards',
+  //     sortable: false
+  //   }, {
+  //     field: 'number_of_replicas',
+  //     name: 'Number of Replicas',
+  //     sortable: false
+  //   }];
+  // }
 
-  componentDidMount() {
-    this.retrieveEntries();
-  }
+  // componentDidMount() {
+  //   this.retrieveEntries();
+  // }
 
-  timeConverter(timestamp) {
-    const x = Number(timestamp);
-    const m = moment(x);
-    return m.format('M/D/YYYY H:mm');
-  }
+  // timeConverter(timestamp) {
+  //   const x = Number(timestamp);
+  //   const m = moment(x);
+  //   return m.format('M/D/YYYY H:mm');
+  // }
 
-  async retrieveEntries() {
-    const self = this;
-    const rows = [];
-    const indexData = await self.genericRequest.request('list_indices', 'GET');
-    Object.keys(indexData).forEach(function (key) {
-      try {
-        const obj = indexData[key];
-        rows.push({
-          index_name: key,
-          state: obj.state === 'open' ? true : false,
-          creation_date: Number(obj.settings.index.creation_date),
-          number_of_shards: obj.settings.index.number_of_shards,
-          number_of_replicas: obj.settings.index.number_of_replicas
-        });
-      } catch (error) {
-        console.error(error);
-      }
-    });
-    self.setState({
-      rows: rows,
-      stillLoading: false
-    });
-    // this.componentWillUpdate();
-  }
+  // async retrieveEntries() {
+  //   const self = this;
+  //   const rows = [];
+  //   const indexData = await self.genericRequest.request('list_indices', 'GET');
+  //   Object.keys(indexData).forEach(function (key) {
+  //     try {
+  //       const obj = indexData[key];
+  //       rows.push({
+  //         index_name: key,
+  //         state: obj.state === 'open' ? true : false,
+  //         creation_date: Number(obj.settings.index.creation_date),
+  //         number_of_shards: obj.settings.index.number_of_shards,
+  //         number_of_replicas: obj.settings.index.number_of_replicas
+  //       });
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   });
+  //   self.setState({
+  //     rows: rows,
+  //     stillLoading: false
+  //   });
+  //   // this.componentWillUpdate();
+  // }
 
 
-  onSelectionChange = (selectedItems) => {
-    this.setState({
-      selectedItems: selectedItems
-    })
-  };
+  // onSelectionChange = (selectedItems) => {
+  //   this.setState({
+  //     selectedItems: selectedItems
+  //   })
+  // };
 
   // onClickSubmit(self) {
   //   if (self.state.selectedItems.length === 0) {
@@ -144,20 +144,21 @@ export class IndexSelection extends Component {
       }
     }
     return (
-      <EuiForm>
+      <div></div>
+      // <EuiForm>
 
-        <EuiInMemoryTable
-          items={this.state.rows}
-          itemId="index_name"
-          loading={this.state.stillLoading}
-          columns={this.columns}
-          pagination={false}
-          selection={selection}
-          isSelectable={true}
-          search={search}
-          sorting={true}
-        />
-      </EuiForm>
+      //   <EuiInMemoryTable
+      //     items={this.state.rows}
+      //     itemId="index_name"
+      //     loading={this.state.stillLoading}
+      //     columns={this.columns}
+      //     pagination={false}
+      //     selection={selection}
+      //     isSelectable={true}
+      //     search={search}
+      //     sorting={true}
+      //   />
+      // </EuiForm>
     );
   }
 }
