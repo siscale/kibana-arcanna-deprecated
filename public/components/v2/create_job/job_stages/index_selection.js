0e92dab9f265218f5bf58e13a24de0c899e54b77
Calin Cornigeanu <alex.cornigeanu@yahoo.com>
2020-03-11 13:41:23 +0200

    adding index selection

--- public/components/v2/create_job/job_stages/index_selection.js
+++ public/components/v2/create_job/job_stages/index_selection.js
@@ -1,18 +1,18 @@
 import React, {
-  Component, Fragment
+  Component, Fragment,
 } from 'react';
 
-// import {
-//   EuiInMemoryTable,
-//   EuiBasicTable,
-//   EuiHealth,
-//   EuiButton,
-//   EuiForm
-// } from '@elastic/eui';
+import {
+  EuiInMemoryTable,
+  EuiBasicTable,
+  EuiHealth,
+  EuiButton,
+  EuiForm
+} from '@elastic/eui';
 
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-// import moment from 'moment';
+import moment from 'moment';
 
 import { GenericRequest } from '../../../../utils/requests';
 
@@ -25,96 +25,96 @@ export class IndexSelection extends Component {
       stillLoading: true,
       selectedItems: []
     };
-    // this.setColumnInfo();
+    this.setColumnInfo();
   }
 
-  // static propTypes = {
-  //   selectedIndexList: PropTypes.array
-  // }
+  static propTypes = {
+    selectedIndexList: PropTypes.array
+  }
 
-  // setColumnInfo() {
-  //   this.columns = [{
-  //     field: 'index_name',
-  //     name: 'Index Name',
-  //     sortable: true
-  //   }, {
-  //     field: 'state',
-  //     name: 'State',
-  //     sortable: true,
-  //     render: (state) => {
-  //       const color = state ? 'success' : 'danger';
-  //       const label = state ? 'Open' : 'Closed';
-  //       return <EuiHealth color={color}>{label}</EuiHealth>;
-  //     }
-  //   }, {
-  //     field: 'creation_date',
-  //     name: 'Creation Date',
-  //     dataType: 'date',
-  //     sortable: true,
-  //     render: (date) => moment(date).toISOString()
-  //   }, {
-  //     field: 'number_of_shards',
-  //     name: 'Number of Shards',
-  //     sortable: false
-  //   }, {
-  //     field: 'number_of_replicas',
-  //     name: 'Number of Replicas',
-  //     sortable: false
-  //   }];
-  // }
+  setColumnInfo() {
+    this.columns = [{
+      field: 'index_name',
+      name: 'Index Name',
+      sortable: true
+    }, {
+      field: 'state',
+      name: 'State',
+      sortable: true,
+      render: (state) => {
+        const color = state ? 'success' : 'danger';
+        const label = state ? 'Open' : 'Closed';
+        return <EuiHealth color={color}>{label}</EuiHealth>;
+      }
+    }, {
+      field: 'creation_date',
+      name: 'Creation Date',
+      dataType: 'date',
+      sortable: true,
+      render: (date) => moment(date).toISOString()
+    }, {
+      field: 'number_of_shards',
+      name: 'Number of Shards',
+      sortable: false
+    }, {
+      field: 'number_of_replicas',
+      name: 'Number of Replicas',
+      sortable: false
+    }];
+  }
 
-  // componentDidMount() {
-  //   this.retrieveEntries();
-  // }
+  componentDidMount() {
+    this.retrieveEntries();
+  }
 
-  // timeConverter(timestamp) {
-  //   const x = Number(timestamp);
-  //   const m = moment(x);
-  //   return m.format('M/D/YYYY H:mm');
-  // }
+  timeConverter(timestamp) {
+    const x = Number(timestamp);
+    const m = moment(x);
+    return m.format('M/D/YYYY H:mm');
+  }
 
-  // async retrieveEntries() {
-  //   const self = this;
-  //   const rows = [];
-  //   const indexData = await self.genericRequest.request('list_indices', 'GET');
-  //   Object.keys(indexData).forEach(function (key) {
-  //     try {
-  //       const obj = indexData[key];
-  //       rows.push({
-  //         index_name: key,
-  //         state: obj.state === 'open' ? true : false,
-  //         creation_date: Number(obj.settings.index.creation_date),
-  //         number_of_shards: obj.settings.index.number_of_shards,
-  //         number_of_replicas: obj.settings.index.number_of_replicas
-  //       });
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   });
-  //   self.setState({
-  //     rows: rows,
-  //     stillLoading: false
-  //   });
-  //   // this.componentWillUpdate();
-  // }
+  async retrieveEntries() {
+    const self = this;
+    const rows = [];
+    const indexData = await self.genericRequest.request('list_indices', 'GET');
+    Object.keys(indexData).forEach(function (key) {
+      try {
+        const obj = indexData[key];
+        rows.push({
+          index_name: key,
+          state: obj.state === 'open' ? true : false,
+          creation_date: Number(obj.settings.index.creation_date),
+          number_of_shards: obj.settings.index.number_of_shards,
+          number_of_replicas: obj.settings.index.number_of_replicas
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    });
+    self.setState({
+      rows: rows,
+      stillLoading: false
+    });
+    // this.componentWillUpdate();
+  }
 
 
-  // onSelectionChange = (selectedItems) => {
-  //   this.setState({
-  //     selectedItems: selectedItems
-  //   })
-  // };
+  onSelectionChange = (selectedItems) => {
+    this.setState({
+      selectedItems: selectedItems
+    })
+  };
 
-  // onClickSubmit(self) {
-  //   if (self.state.selectedItems.length === 0) {
-  //     return;
-  //   }
-  //   self.props.selectedIndexList.length = 0;
-  //   self.state.selectedItems.forEach((element) => {
-  //     self.props.selectedIndexList.push(element);
-  //   });
-  //   window.location.href = '#/create_job_mappings';
-  // }
+  onClickSubmit(self) {
+    if (self.state.selectedItems.length === 0) {
+      return;
+    }
+    self.props.selectedIndexList.length = 0;
+    self.state.selectedItems.forEach((element) => {
+      self.props.selectedIndexList.push(element);
+    });
+    window.location.href = '#/create_job_mappings';
+  }
 
   renderSubmitButton() {
     const self = this;
@@ -122,12 +122,12 @@ export class IndexSelection extends Component {
     if (selection.length === 0) {
       return;
     }
-    return;
-    // return (
-    //   <EuiButton onClick={() => this.onClickSubmit(self)}>
-    //     Select {selection.length} indices
-    //   </EuiButton>
-    // );
+
+    return (
+      <EuiButton onClick={() => this.onClickSubmit(self)}>
+        Select {selection.length} indices
+      </EuiButton>
+    );
   }
 
   render() {
@@ -144,21 +144,20 @@ export class IndexSelection extends Component {
       }
     }
     return (
-      <div></div>
-      // <EuiForm>
+      <EuiForm>
 
-      //   <EuiInMemoryTable
-      //     items={this.state.rows}
-      //     itemId="index_name"
-      //     loading={this.state.stillLoading}
-      //     columns={this.columns}
-      //     pagination={false}
-      //     selection={selection}
-      //     isSelectable={true}
-      //     search={search}
-      //     sorting={true}
-      //   />
-      // </EuiForm>
+        <EuiInMemoryTable
+          items={this.state.rows}
+          itemId="index_name"
+          loading={this.state.stillLoading}
+          columns={this.columns}
+          pagination={false}
+          selection={selection}
+          isSelectable={true}
+          search={search}
+          sorting={true}
+        />
+      </EuiForm>
     );
   }
 }
