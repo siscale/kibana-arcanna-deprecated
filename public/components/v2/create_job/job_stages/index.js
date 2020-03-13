01cdf65ad14784233142cff8dda62c1e65122282
Calin Cornigeanu <alex.cornigeanu@yahoo.com>
2020-03-13 18:01:21 +0200

    adding mapping selection create job

--- public/components/v2/create_job/job_stages/index.js
+++ public/components/v2/create_job/job_stages/index.js
@@ -1,2 +1,2 @@
-export {IndexSelection} from './index_selection';
-export {MappingSelection} from './mapping_selection';
+export {IndexSelection} from './index_selection/index_selection';
+export {MappingSelection} from './mapping_selection/mapping_selection';
