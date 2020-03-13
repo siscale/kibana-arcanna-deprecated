268ff49250d0c44a8f6228b132a3cfa080d4367d
Calin Cornigeanu <alex.cornigeanu@yahoo.com>
2020-03-13 21:24:48 +0200

    testing redirect to home

--- public/components/v2/list_jobs/job_header.js
+++ public/components/v2/list_jobs/job_header.js
@@ -34,7 +34,7 @@ export class JobListHeader extends React.Component {
 
   onClickCreateJob = () => {
     this.props.history.push('create_job');
-    this.componentWillUnmount();
+    // this.componentWillUnmount();
   }
 
 
